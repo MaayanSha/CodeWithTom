700f541964beb3074923f31bf33394c26a9867e9
maayan <maayansh.ms@gmail.com>
2024-02-07 17:38:51 +0200

    final check, slight UI improvements, ready for deployment

--- code-with-tom/src/index.js
+++ code-with-tom/src/index.js
@@ -7,6 +7,11 @@ import {Provider} from "react-redux";
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {store} from "./store/store";
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === 'production') {
+    disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
