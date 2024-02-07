46a9ecb03a3c2c5402ba1abd27b4f30bebee625e
maayan <maayansh.ms@gmail.com>
2024-02-07 20:35:04 +0200

    final deploy

--- src/components/code/CodeSandbox.js
+++ src/components/code/CodeSandbox.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import "highlight.js/styles/base16/espresso.css";
 import Editor from '@monaco-editor/react';
